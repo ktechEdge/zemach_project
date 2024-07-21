d5e8bdade84f546c33b49f2998fc562158d19dc1
Yanal Abzak <131192823+abzakcd@users.noreply.github.com>
2024-07-21 18:43:47 +0300

    environmental cnt

--- middleware/environmental.js
+++ middleware/environmental.js
@@ -1,4 +1,5 @@
 
+
 const getAllData = (callback) => {
     dbPool.query('SELECT * FROM environmental_data', (err, rows) => {
         if (err) return callback(err);
@@ -14,152 +15,70 @@ const getDataById = (id, callback) => {
 };
 
 const createData = (data, callback) => {
-    const { device_id, uv_radiation, light, air_temperature, air_humidity, soil_humidity, measurement_date } = data;
+    const {
+        device_id, uv_radiation, uv_radiation_max, uv_radiation_min,
+        light, light_max, light_min, air_temperature, air_temperature_max,
+        air_temperature_min, air_humidity, air_humidity_max, air_humidity_min,
+        soil_humidity, soil_humidity_max, soil_humidity_min, plant_ID, measurement_date
+    } = data;
 
-    dbPool.query('SELECT * FROM environmental_data WHERE device_id = ? AND DATE(measurement_date) = DATE(?)', [device_id, measurement_date], (err, existingData) => {
+
+    // Check for the current cnt value
+    dbPool.query('SELECT * FROM environmental_data', (err, result) => {
         if (err) return callback(err);
 
-        let sumFields = {
-            uv_radiation: uv_radiation,
-            light: light,
-            air_temperature: air_temperature,
-            air_humidity: air_humidity,
-            soil_humidity: soil_humidity
-        };
+        let cnt = result.length; // Default cnt value
+        cnt ++; // Increment cnt value
 
-        let maxFields = {
-            uv_radiation_max: uv_radiation,
-            light_max: light,
-            air_temperature_max: air_temperature,
-            air_humidity_max: air_humidity,
-            soil_humidity_max: soil_humidity
-        };
-
-        let minFields = {
-            uv_radiation_min: uv_radiation,
-            light_min: light,
-            air_temperature_min: air_temperature,
-            air_humidity_min: air_humidity,
-            soil_humidity_min: soil_humidity
-        };
-
-        if (existingData.length > 0) {
-            sumFields = {
-                uv_radiation: existingData[0].uv_radiation + uv_radiation,
-                light: existingData[0].light + light,
-                air_temperature: existingData[0].air_temperature + air_temperature,
-                air_humidity: existingData[0].air_humidity + air_humidity,
-                soil_humidity: existingData[0].soil_humidity + soil_humidity
-            };
-
-            maxFields = {
-                uv_radiation_max: Math.max(existingData[0].uv_radiation_max, uv_radiation),
-                light_max: Math.max(existingData[0].light_max, light),
-                air_temperature_max: Math.max(existingData[0].air_temperature_max, air_temperature),
-                air_humidity_max: Math.max(existingData[0].air_humidity_max, air_humidity),
-                soil_humidity_max: Math.max(existingData[0].soil_humidity_max, soil_humidity)
-            };
-
-            minFields = {
-                uv_radiation_min: Math.min(existingData[0].uv_radiation_min, uv_radiation),
-                light_min: Math.min(existingData[0].light_min, light),
-                air_temperature_min: Math.min(existingData[0].air_temperature_min, air_temperature),
-                air_humidity_min: Math.min(existingData[0].air_humidity_min, air_humidity),
-                soil_humidity_min: Math.min(existingData[0].soil_humidity_min, soil_humidity)
-            };
-
-            dbPool.query('UPDATE environmental_data SET ? WHERE id = ?', [
-                {
-                    ...sumFields,
-                    ...maxFields,
-                    ...minFields,
-                    cnt: existingData[0].cnt + 1
-                },
-                existingData[0].id
-            ], (err) => {
-                if (err) return callback(err);
-                // בדיקה אם הדקה היא כפולה שלמה של 10
-                const currentMinute = new Date(measurement_date).getMinutes();
-                if (currentMinute % 10 === 0) {
-                    calculateAndStoreAverages(device_id, callback);
-                } else {
-                    callback(null, true);
-                }
-            });
-        } else {
-            dbPool.query('INSERT INTO environmental_data SET ?', {
-                device_id,
-                ...sumFields,
-                ...maxFields,
-                ...minFields,
-                cnt: 1,
+        const query = `
+            INSERT INTO environmental_data (
+                device_id, uv_radiation, uv_radiation_max, uv_radiation_min,
+                light, light_max, light_min, air_temperature, air_temperature_max,
+                air_temperature_min, air_humidity, air_humidity_max, air_humidity_min,
+                soil_humidity, soil_humidity_max, soil_humidity_min, plant_ID, cnt,
                 measurement_date
-            }, (err, result) => {
-                if (err) return callback(err);
-                // בדיקה אם הדקה היא כפולה שלמה של 10
-                const currentMinute = new Date(measurement_date).getMinutes();
-                if (currentMinute % 10 === 0) {
-                    calculateAndStoreAverages(device_id, callback);
-                } else {
-                    callback(null, result.insertId);
-                }
-            });
-        }
-    });
-};
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        `;
 
-const calculateAndStoreAverages = (device_id, callback) => {
-    dbPool.query('SELECT * FROM environmental_data WHERE device_id = ?', [device_id], (err, data) => {
-        if (err) return callback(err);
+        const values = [
+            device_id, uv_radiation, uv_radiation_max, uv_radiation_min,
+            light, light_max, light_min, air_temperature, air_temperature_max,
+            air_temperature_min, air_humidity, air_humidity_max, air_humidity_min,
+            soil_humidity, soil_humidity_max, soil_humidity_min, plant_ID, cnt, measurement_date
+        ];
 
-        if (data.length > 0) {
-            const averages = {
-                uv_radiation_avg: data[0].uv_radiation / data[0].cnt,
-                light_avg: data[0].light / data[0].cnt,
-                air_temperature_avg: data[0].air_temperature / data[0].cnt,
-                air_humidity_avg: data[0].air_humidity / data[0].cnt,
-                soil_humidity_avg: data[0].soil_humidity / data[0].cnt,
-            };
-
-            const maxFields = {
-                uv_radiation_max: data[0].uv_radiation_max,
-                light_max: data[0].light_max,
-                air_temperature_max: data[0].air_temperature_max,
-                air_humidity_max: data[0].air_humidity_max,
-                soil_humidity_max: data[0].soil_humidity_max
-            };
-
-            const minFields = {
-                uv_radiation_min: data[0].uv_radiation_min,
-                light_min: data[0].light_min,
-                air_temperature_min: data[0].air_temperature_min,
-                air_humidity_min: data[0].air_humidity_min,
-                soil_humidity_min: data[0].soil_humidity_min
-            };
-
-            dbPool.query('INSERT INTO environmental_data_history SET ?', {
-                device_id,
-                ...averages,
-                ...maxFields,
-                ...minFields,
-                measurement_date: new Date()
-            }, (err) => {
-                if (err) return callback(err);
-                // איפוס הנתונים לאחר שמירת ההיסטוריה
-                dbPool.query('UPDATE environmental_data SET uv_radiation = 0, light = 0, air_temperature = 0, air_humidity = 0, soil_humidity = 0, cnt = 0 WHERE device_id = ?', [device_id], (err) => {
-                    if (err) return callback(err);
-                    callback(null, true);
-                });
-            });
-        } else {
-            callback(null, true);
-        }
+        dbPool.query(query, values, (err, result) => {
+            if (err) return callback(err);
+            callback(null, result.insertId);
+        });
     });
 };
 
 const updateData = (id, data, callback) => {
-    const { device_id, uv_radiation, uv_radiation_max, uv_radiation_min, light, light_max, light_min, air_temperature, air_temperature_max, air_temperature_min, air_humidity, air_humidity_max, air_humidity_min, soil_humidity, soil_humidity_max, soil_humidity_min, plant_ID, cnt, measurement_date } = data;
-    dbPool.query('UPDATE environmental_data SET device_id = ?, uv_radiation = ?, uv_radiation_max = ?, uv_radiation_min = ?, light = ?, light_max = ?, light_min = ?, air_temperature = ?, air_temperature_max = ?, air_temperature_min = ?, air_humidity = ?, air_humidity_max = ?, air_humidity_min = ?, soil_humidity = ?, soil_humidity_max = ?, soil_humidity_min = ?, plant_ID = ?, cnt = ?, measurement_date = ? WHERE id = ?', [device_id, uv_radiation, uv_radiation_max, uv_radiation_min, light, light_max, light_min, air_temperature, air_temperature_max, air_temperature_min, air_humidity, air_humidity_max, air_humidity_min, soil_humidity, soil_humidity_max, soil_humidity_min, plant_ID, cnt, measurement_date, id], callback);
+    const {
+        device_id, uv_radiation, uv_radiation_max, uv_radiation_min,
+        light, light_max, light_min, air_temperature, air_temperature_max,
+        air_temperature_min, air_humidity, air_humidity_max, air_humidity_min,
+        soil_humidity, soil_humidity_max, soil_humidity_min, plant_ID, cnt, measurement_date
+    } = data;
+
+    const query = `
+        UPDATE environmental_data SET
+            device_id = ?, uv_radiation = ?, uv_radiation_max = ?, uv_radiation_min = ?,
+            light = ?, light_max = ?, light_min = ?, air_temperature = ?, air_temperature_max = ?,
+            air_temperature_min = ?, air_humidity = ?, air_humidity_max = ?, air_humidity_min = ?,
+            soil_humidity = ?, soil_humidity_max = ?, soil_humidity_min = ?, plant_ID = ?,
+            cnt = ?, measurement_date = ?
+        WHERE id = ?
+    `;
+    const values = [
+        device_id, uv_radiation, uv_radiation_max, uv_radiation_min,
+        light, light_max, light_min, air_temperature, air_temperature_max,
+        air_temperature_min, air_humidity, air_humidity_max, air_humidity_min,
+        soil_humidity, soil_humidity_max, soil_humidity_min, plant_ID, cnt, measurement_date, id
+    ];
+
+    dbPool.query(query, values, callback);
 };
 
 const deleteData = (id, callback) => {
--- routes/environmental.js
+++ routes/environmental.js
@@ -2,25 +2,7 @@ const express = require('express');
 const router = express.Router();
 const environmentalDataMiddleware = require('../middleware/environmental');
 
-/**
- * @swagger
- * tags:
- *   name: EnvironmentalData
- *   description: Environmental data management
- */
-
-/**
- * @swagger
- * /environmental-data:
- *   get:
- *     summary: Get all environmental data
- *     tags: [EnvironmentalData]
- *     responses:
- *       200:
- *         description: List of all environmental data
- *       500:
- *         description: Error retrieving environmental data
- */
+// Get all environmental data
 router.get('/', (req, res) => {
     environmentalDataMiddleware.getAllData((err, data) => {
         if (err) {
@@ -31,26 +13,7 @@ router.get('/', (req, res) => {
     });
 });
 
-/**
- * @swagger
- * /environmental-data/{id}:
- *   get:
- *     summary: Get environmental data by ID
- *     tags: [EnvironmentalData]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *     responses:
- *       200:
- *         description: Environmental data by ID
- *       404:
- *         description: Environmental data not found
- *       500:
- *         description: Error retrieving environmental data
- */
+// Get environmental data by ID
 router.get('/:id', (req, res) => {
     environmentalDataMiddleware.getDataById(req.params.id, (err, data) => {
         if (err) {
@@ -64,39 +27,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
-/**
- * @swagger
- * /environmental-data:
- *   post:
- *     summary: Create new environmental data
- *     tags: [EnvironmentalData]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               device_id:
- *                 type: integer
- *               uv_radiation:
- *                 type: integer
- *               light:
- *                 type: integer
- *               air_temperature:
- *                 type: number
- *               air_humidity:
- *                 type: number
- *               soil_humidity:
- *                 type: integer
- *               measurement_date:
- *                 type: string
- *     responses:
- *       201:
- *         description: The created environmental data.
- *       500:
- *         description: Error creating environmental data
- */
+// Create new environmental data
 router.post('/', (req, res) => {
     environmentalDataMiddleware.createData(req.body, (err, id) => {
         if (err) {
@@ -107,47 +38,7 @@ router.post('/', (req, res) => {
     });
 });
 
-/**
- * @swagger
- * /environmental-data/{id}:
- *   put:
- *     summary: Update environmental data by ID
- *     tags: [EnvironmentalData]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               device_id:
- *                 type: integer
- *               uv_radiation:
- *                 type: integer
- *               light:
- *                 type: integer
- *               air_temperature:
- *                 type: number
- *               air_humidity:
- *                 type: number
- *               soil_humidity:
- *                 type: integer
- *               measurement_date:
- *                 type: string
- *     responses:
- *       200:
- *         description: The updated environmental data
- *       404:
- *         description: Environmental data not found
- *       500:
- *         description: Error updating environmental data
- */
+// Update environmental data by ID
 router.put('/:id', (req, res) => {
     environmentalDataMiddleware.updateData(req.params.id, req.body, (err) => {
         if (err) {
@@ -158,26 +49,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-/**
- * @swagger
- * /environmental-data/{id}:
- *   delete:
- *     summary: Delete environmental data by ID
- *     tags: [EnvironmentalData]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *     responses:
- *       200:
- *         description: The deleted environmental data
- *       404:
- *         description: Environmental data not found
- *       500:
- *         description: Error deleting environmental data
- */
+// Delete environmental data by ID
 router.delete('/:id', (req, res) => {
     environmentalDataMiddleware.deleteData(req.params.id, (err) => {
         if (err) {
