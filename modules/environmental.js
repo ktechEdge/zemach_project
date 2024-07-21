8ba413eb26febce857ee02807ffd8304d9bdfe77
Yanal Abzak <131192823+abzakcd@users.noreply.github.com>
2024-07-21 04:44:21 +0300

    arduino new Rout_middle

--- modules/environmental.js
+++ modules/environmental.js
@@ -1,34 +1,35 @@
-// modules/environmental.js
 
-const createEnvironmentalData = (data, callback) => {
-    const sql = 'INSERT INTO environmental_data SET ?';
-    dbPool.query(sql, data, callback);
+const getAllData = async () => {
+    const [rows] = await dbPool.query('SELECT * FROM environmental_data');
+    return rows;
 };
 
-const getAllEnvironmentalData = (callback) => {
-    const sql = 'SELECT * FROM environmental_data';
-    dbPool.query(sql, callback);
+const getDataById = async (id) => {
+    const [rows] = await dbPool.query('SELECT * FROM environmental_data WHERE id = ?', [id]);
+    return rows[0];
 };
 
-const getEnvironmentalDataById = (id, callback) => {
-    const sql = 'SELECT * FROM environmental_data WHERE id = ?';
-    dbPool.query(sql, [id], callback);
+const createData = async (data) => {
+    const { device_id, uv_radiation, uv_radiation_max, uv_radiation_min, light, light_max, light_min, air_temperature, air_temperature_max, air_temperature_min, air_humidity, air_humidity_max, air_humidity_min, soil_humidity, soil_humidity_max, soil_humidity_min, plant_ID, cnt, measurement_date } = data;
+    const [result] = await dbPool.query('INSERT INTO environmental_data (device_id, uv_radiation, uv_radiation_max, uv_radiation_min, light, light_max, light_min, air_temperature, air_temperature_max, air_temperature_min, air_humidity, air_humidity_max, air_humidity_min, soil_humidity, soil_humidity_max, soil_humidity_min, plant_ID, cnt, measurement_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [device_id, uv_radiation, uv_radiation_max, uv_radiation_min, light, light_max, light_min, air_temperature, air_temperature_max, air_temperature_min, air_humidity, air_humidity_max, air_humidity_min, soil_humidity, soil_humidity_max, soil_humidity_min, plant_ID, cnt, measurement_date]);
+    return result.insertId;
 };
 
-const updateEnvironmentalData = (id, data, callback) => {
-    const sql = 'UPDATE environmental_data SET ? WHERE id = ?';
-    dbPool.query(sql, [data, id], callback);
+const updateData = async (id, data) => {
+    const { device_id, uv_radiation, uv_radiation_max, uv_radiation_min, light, light_max, light_min, air_temperature, air_temperature_max, air_temperature_min, air_humidity, air_humidity_max, air_humidity_min, soil_humidity, soil_humidity_max, soil_humidity_min, plant_ID, cnt, measurement_date } = data;
+    await dbPool.query('UPDATE environmental_data SET device_id = ?, uv_radiation = ?, uv_radiation_max = ?, uv_radiation_min = ?, light = ?, light_max = ?, light_min = ?, air_temperature = ?, air_temperature_max = ?, air_temperature_min = ?, air_humidity = ?, air_humidity_max = ?, air_humidity_min = ?, soil_humidity = ?, soil_humidity_max = ?, soil_humidity_min = ?, plant_ID = ?, cnt = ?, measurement_date = ? WHERE id = ?', [device_id, uv_radiation, uv_radiation_max, uv_radiation_min, light, light_max, light_min, air_temperature, air_temperature_max, air_temperature_min, air_humidity, air_humidity_max, air_humidity_min, soil_humidity, soil_humidity_max, soil_humidity_min, plant_ID, cnt, measurement_date, id]);
+    return true;
 };
 
-const deleteEnvironmentalData = (id, callback) => {
-    const sql = 'DELETE FROM environmental_data WHERE id = ?';
-    dbPool.query(sql, [id], callback);
+const deleteData = async (id) => {
+    await dbPool.query('DELETE FROM environmental_data WHERE id = ?', [id]);
+    return true;
 };
 
 module.exports = {
-    createEnvironmentalData,
-    getAllEnvironmentalData,
-    getEnvironmentalDataById,
-    updateEnvironmentalData,
-    deleteEnvironmentalData
+    getAllData,
+    getDataById,
+    createData,
+    updateData,
+    deleteData
 };
