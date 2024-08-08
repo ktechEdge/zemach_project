d9d9531af0170c200deff5ab942041359d1b937c
nati-pinyan <natipinyan@gmail.com>
2024-08-08 18:59:13 +0300

    need fix the mw function

--- routes/data.js
+++ routes/data.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/GetAllData', (req, res) => {
-    const query = 'SELECT * FROM environmental_data_avg';
-    dbPool.query(query, (err, results) => {
-        if (err) {
-            console.error('Error executing query:', err);
-            res.status(500).json({ error: 'An error occurred while fetching data' });
-        } else {
-            res.json(results);
-        }
-    });
-});
+const environmentalAvgMiddleware = require('../middleware/data');
 
+router.get('/GetAllData', async (req, res) => {
+    try {
+        //const { startDate, endDate } = req.query;
+        const  startDate = '2024-08-25';
+        const endDate =  '2024-11-25 23:59:59';
+        const data = await environmentalAvgMiddleware.GetData(startDate, endDate);
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 console.log(`Now listening on port http://localhost:${4286}/data/GetAllData`);
 module.exports = router; //stam
