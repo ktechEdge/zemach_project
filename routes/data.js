e5aaa8222f7069a34c9aa2a845f7c899bcccdaff
dorel <dorelhayani@gmail.com>
2024-08-22 17:57:20 +0300

    stam

--- routes/data.js
+++ routes/data.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
 const environmentalAvgMiddleware = require('../middleware/data');
 
 router.get('/GetAllData', async (req, res) => {
     try {
-        //const { startDate, endDate } = req.query;
-        const  startDate = '2024-08-25';
-        const endDate =  '2024-11-25 23:59:59';
+        const { startDate, endDate } = req.query;
+        // const  startDate = '2024-08-25';
+        // const endDate =  '2024-11-25 23:59:59';
         const data = await environmentalAvgMiddleware.GetData(startDate, endDate);
         res.json(data);
     } catch (error) {
