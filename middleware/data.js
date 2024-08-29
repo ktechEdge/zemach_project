ed751b41424f69b1b0b917efd157f5559bcacf27
nati-pinyan <natipinyan@gmail.com>
2024-08-29 19:22:25 +0300

    The arrangement of the function to pull the information from the data base

--- middleware/data.js
+++ middleware/data.js
@@ -1,17 +1,26 @@
-const GetData = (startDate, endDate) => {
-    return new Promise((resolve, reject) => {
-        const query = 'SELECT * FROM environmental_data_avg WHERE measurement_date BETWEEN ? AND ?';
-        const params = [startDate, endDate];
+const GetData = async (req, res, next) => {
+    try {
+        //const { startDate = '2024-08-25', endDate = '2024-11-25 23:59:59' } = req.query;
+        //default values for the query variables
+        const { startDate, endDate } = req.body;
+        const query = `SELECT * FROM environmental_data_avg WHERE measurement_date BETWEEN '${startDate}' AND '${endDate}'`;
 
-        dbPool.query(query, params, (err, results) => {
-            if (err) {
-                console.error('Error executing query:', err);
-                reject(new Error('An error occurred while fetching data'));
-            } else {
-                resolve(results);
-            }
-        });
-    });
+        const promisePool = dbPool.promise();
+
+        try {
+            const [result] = await promisePool.query(query);
+            req.result = result;
+        } catch (err) {
+            console.error('Error executing query:', err);
+            req.error = new Error('An error occurred while fetching data');
+        }
+
+        next();
+    } catch (error) {
+        console.error('Unexpected error:', error);
+        req.error = new Error('Unexpected error occurred');
+        next();
+    }
 };
 
 module.exports = {
--- routes/data.js
+++ routes/data.js
@@ -3,16 +3,15 @@ const router = express.Router();
 
 const environmentalAvgMiddleware = require('../middleware/data');
 
-router.get('/GetAllData', async (req, res) => {
-    try {
-        //const { startDate, endDate } = req.query;
-        const  startDate = '2024-08-25';
-        const endDate =  '2024-11-25 23:59:59';
-        const data = await environmentalAvgMiddleware.GetData(startDate, endDate);
-        res.json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+
+router.get('/GetAllData', environmentalAvgMiddleware.GetData, (req, res) => {
+    if (req.error) {
+        res.status(500).json({ error: req.error.message });
+    } else {
+        res.json(req.result);
     }
 });
+
+
 console.log(`Now listening on port http://localhost:${4286}/data/GetAllData`);
-module.exports = router; //stam
+module.exports = router;
