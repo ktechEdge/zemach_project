74b3c3f3535cdf2bb7621f14018d899d661993ff
Tks23-hub <forcodin23@gmail.com>
2024-09-19 18:39:57 +0300

    Updating the same files to make a one code for all the front workers And Creating A page that allows the workers to add notes the page name is Workers_Notes.ejs it has javascript seperate page and css

---
+++ routes/FE_R.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+module.exports = router;
+
+router.get("/",(req, res) => {
+    res.render("demo_page",{pageTitle:"Home Page"});
+});
+
+router.get("/Plants_Statuses",(req, res) => {
+    res.render("Plants_Status",{pageTitle:"Plants Status"});
+});
+
+router.get("/plant_Status_history",(req, res) => {
+    res.render("plant_Status_history",{pageTitle:"plant history"});
+});
+
+router.get("/Devices_Location",(req, res) => {
+    res.render("Devices_Location",{pageTitle:"Device Location"});
+});
+
+router.get("/History_Graph",(req, res) => {
+    res.render("History_Graph.ejs",{pageTitle:"plant history Graph"});
+});
+
+router.get("/Workers_Notes",(req, res) => {
+    res.render("Workers_Notes.ejs",{pageTitle:"Workers Notes"});
+});
